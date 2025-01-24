8621667fa47d61377d4088ef20cd6316a63cfb2b
anasmaimalee <anasment6@gmail.com>
2025-01-24 16:40:44 +0100

    fix(register):page

--- register.php
+++ register.php
@@ -21,64 +21,101 @@ require './inc/lib.links.php';
     <div class="authincation h-100">
         <div class="container-fluid h-100">
             <div class="row justify-content-center h-100 align-items-center">
-                <div class="col-md-6">
+                <div class="col-md-4">
                     <div class="authincation-content">
-                        <div class="row no-gutters">
-                            <div class="col-xl-12">
-                                <div class="auth-form">
-                                    <h4 class="text-center mb-4">Create New  Account</h4>
-                                    <form action=""  id="reg_form">
-                                    <div class="form-group">
-                                            <label><strong>First Name</strong></label>
-                                            <input type="text" class="form-control" placeholder="First name" name="first_name">
+                    <div class="row no-gutters">
+                    <div class="col-xl-12">
+                        <div class="auth-form">
+                            <h4 class="text-center mb-4">Create New Account</h4>
+                            <form action="" id="reg_form">
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text"><i class="fa fa-user"></i></span>
                                         </div>
-                                        <div class="form-group">
-                                            <label><strong>Last Name</strong></label>
-                                            <input type="text" class="form-control" placeholder="Last name" name="last_name">
-                                        </div>
-                                        <div class="form-group">
-                                            <label><strong>Username</strong></label>
-                                            <input type="text" class="form-control" placeholder="username" name="username">
-                                        </div>
-                                        <div class="form-group">
-                                            <label><strong>Email</strong></label>
-                                            <input type="email" class="form-control" placeholder="hello@example.com" name="email">
-                                        </div>
-                                        <div class="form-group">
-                                            <label><strong>Phone Number</strong></label>
-                                            <input type="phone" class="form-control" placeholder="Phone Number" name="phone">
-                                        </div>
-                                        <div class="form-group">
-                                            <label><strong>State</strong></label>
-                                            <input type="text" class="form-control" placeholder="State" name="state">
-                                        </div>
-                                   
-                                        <div class="form-group">
-                                            <label><strong>User Tyoe</strong></label>
-                                            <select class="form-control"  name="user_type">
-                                                <option value="user_type">user type</option>
-                                                <option  value="student">Student</option>
-                                                <option value="cafe_owner">Cafe owner</option>
-                                            </select>
-                                        </div>
-                                        <div class="form-group">
-                                            <label><strong>Password</strong></label>
-                                            <input type="password" class="form-control" name="password">
-                                        </div>
-                                        <div class="form-group">
-                                            <label><strong>Confirm Password</strong></label>
-                                            <input type="password" class="form-control" name="confirm_password">
-                                        </div>
-                                        <div class="text-center mt-4">
-                                            <button type="submit" class="btn btn-primary btn-block">Sign me up</button>
-                                        </div>
-                                    </form>
-                                    <div class="new-account mt-3">
-                                        <p>Already have an account? <a class="text-primary" href="login">Sign in</a></p>
+                                        <input type="text" class="form-control" placeholder="First name" name="first_name">
                                     </div>
                                 </div>
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text"><i class="fa fa-user"></i></span>
+                                        </div>
+                                        <input type="text" class="form-control" placeholder="Last name" name="last_name">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text"><i class="fa fa-user-circle"></i></span>
+                                        </div>
+                                        <input type="text" class="form-control" placeholder="Username" name="username">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text"><i class="fa fa-envelope"></i></span>
+                                        </div>
+                                        <input type="email" class="form-control" placeholder="hello@example.com" name="email">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text"><i class="fa fa-phone"></i></span>
+                                        </div>
+                                        <input type="phone" class="form-control" placeholder="Phone Number" name="phone">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text"><i class="fa fa-map-marker-alt"></i></span>
+                                        </div>
+                                        <input type="text" class="form-control" placeholder="State" name="state">
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text"><i class="fa fa-user-tag"></i></span>
+                                        </div>
+                                        <select class="form-control" name="user_type">
+                                            <option value="user_type">User type</option>
+                                            <option value="student">Student</option>
+                                            <option value="cafe_owner">Cafe Owner</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text"><i class="fa fa-lock"></i></span>
+                                        </div>
+                                        <input type="password" class="form-control" placeholder="Password" name="password">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text"><i class="fa fa-lock"></i></span>
+                                        </div>
+                                        <input type="password" class="form-control" placeholder="Confirm Password" name="confirm_password">
+                                    </div>
+                                </div>
+                                <div class="text-center mt-4">
+                                    <button type="submit" class="btn btn-primary btn-block">Sign me up</button>
+                                </div>
+                            </form>
+                            <div class="new-account mt-3">
+                                <p>Already have an account? <a class="text-primary" href="login">Sign in</a></p>
                             </div>
                         </div>
+                    </div>
+</div>
+
                     </div>
                 </div>
             </div>
